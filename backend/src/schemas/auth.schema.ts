bd855ccbdf62c9c158dfee74a4dccd4954ed09ee
Diego Barbosa <diegobarbosarj20@gmail.com>
2026-06-17 00:35:24 -0300

    feat: add Amadeus API integration and enhance validation messages in auth schemas

--- backend/src/schemas/auth.schema.ts
+++ backend/src/schemas/auth.schema.ts
@@ -3,19 +3,19 @@ import * as z from "zod";
 export const loginSchema = z.object({
   email: z.string().email("Email inválido"),
   password: z
-    .string()
+    .string("A senha é obrigatória")
     .min(6, "A senha deve ter pelo menos 6 caracteres")
     .max(16, "A senha deve ter no máximo 16 caracteres"),
 });
 
 export const registerSchema = z.object({
   name: z
-    .string()
+    .string("O nome é obrigatório")
     .min(3, "O nome deve ter pelo menos 3 caracteres")
     .max(24, "O nome deve ter no máximo 24 caracteres"),
   email: z.string().email("Email inválido"),
   password: z
-    .string()
+    .string("A senha é obrigatória")
     .min(6, "A senha deve ter pelo menos 6 caracteres")
     .max(16, "A senha deve ter no máximo 16 caracteres"),
 });
